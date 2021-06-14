c1aaca23598b7b3f31731464d8816d4862ce6892
Daisuke <kurimotodaisuke@DaisukenoMacBook-Air.local>
2021-06-14 22:09:24 +0900

    create article model

---
+++ db/migrate/20210614130625_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.integer :status, default: 0
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
