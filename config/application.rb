84e1068f36d04d8ff96127e97e82fbd7c567e75b
Daisuke <kurimotodaisuke@DaisukenoMacBook-Air.local>
2021-06-15 17:04:16 +0900

    rack cosの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,14 @@ module QiitaClone
     config.api_only = true
     config.middleware.use ActionDispatch::Flash
     config.generators.system_tests = nil
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+                 headers: :any,
+                 expose: ["access-token", "expiry", "token-type", "uid", "client"],
+                 methods: [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
