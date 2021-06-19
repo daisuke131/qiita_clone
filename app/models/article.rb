5cf5edc09eacc29d198345aec54ef5f1952391a6
Daisuke <kurimotodaisuke@DaisukenoMacBook-Air.local>
2021-06-19 15:29:33 +0900

    likeテーブルと紐付け

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
   validates :title, :body, presence: true
   enum status: { draft: 0, published: 1 }
 end
